43caf790b26fdfaf1a749c4ecfcb90a989f3ebc6
Shane Osbourne <shane.osbourne8@gmail.com>
2017-04-19 08:16:20 +0100

    Added test for /system -> child -> child -> child actor

--- test/system.test.js
+++ test/system.test.js
@@ -103,3 +103,60 @@ it('/system/actor -> child -> child', function () {
     assert.equal(register['/system/FileWatcher'].type, 'FileWatcher');
     assert.equal(register['/system/FileWatcher'].actors[0].address, '/system/FileWatcher/sub-path-01');
 });
+
+it('/system/actor -> child -> child -> child', function () {
+    const scheduler = new TestScheduler();
+    const system = createSystem({
+        messageScheduler: scheduler
+    });
+    const GrandchildActor = class {
+        constructor(address) {
+            this.type        = 'GrandchildActor';
+            this.mailboxType = 'default';
+            this.address = address;
+        }
+        // todo - sender here should also be an actor ref
+        receive(payload, message, sender) {
+            assert.equal(payload, 'Hey from the grandchild!');
+        }
+    };
+    const ChildActor = class {
+        constructor(address, context) {
+            this.type        = 'ChildActor';
+            this.mailboxType = 'default';
+            this.address = address;
+            this.actors = [
+                context.actorOf(GrandchildActor)
+            ];
+            this.actors[0].tell('Hey from the grandchild!')
+                .subscribe();
+        }
+        // todo - sender here should also be an actor ref
+        receive(payload, message, sender) {
+            // console.log('Re'
+        }
+    };
+    const SystemLevelActor = class {
+        constructor(address, context) {
+            this.type        = 'SystemLevelActor';
+            this.mailboxType = 'default';
+            this.address     = address;
+            this.actors      = [];
+            this.context     = context;
+        }
+        receive(payload) {
+            switch(payload) {
+                case 'init':
+                    this.actors.push(this.context.actorOf(ChildActor, 'sub-path-01'));
+            }
+        }
+    };
+    const actor = system.actorOf(SystemLevelActor, 'SystemLevelActor');
+    assert.equal(actor.address, '/system/SystemLevelActor');
+    actor.tell('init').subscribe();
+    scheduler.flush();
+
+    const register = system.actorRegister.getValue();
+    assert.equal(register['/system/SystemLevelActor'].type, 'SystemLevelActor');
+    assert.equal(register['/system/SystemLevelActor'].actors[0].address, '/system/SystemLevelActor/sub-path-01');
+});
