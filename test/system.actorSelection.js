55845d145ec51323e7090a28badbaf226ea8d3e6
Shane Osbourne <shane.osbourne8@gmail.com>
2017-04-21 07:46:07 +0100

    [wip] adding test for multiple selections via actorSelection()

--- test/system.actorSelection.js
+++ test/system.actorSelection.js
@@ -23,3 +23,32 @@ it('can select actors using actorSelection as system level', function () {
     selected[0].tell('ping').subscribe();
     scheduler.flush();
 });
+it('can select mulitple actors at system level using glob', function () {
+    const scheduler = new TestScheduler();
+    const system = createSystem({
+        messageScheduler: scheduler
+    });
+    const Guardian = class {
+        constructor(address) {
+            this.type = 'Guardian';
+            this.mailboxType = 'default';
+        }
+        receive(payload) {
+            assert.equal(payload, 'ping');
+        }
+    };
+    const Guardian2 = class {
+        constructor(address) {
+            this.type = 'Guardian2';
+            this.mailboxType = 'default';
+        }
+        receive(payload) {
+            assert.equal(payload, 'ping ping');
+        }
+    };
+    const actor = system.actorOf(Guardian, 'guardian-actor-01');
+    const selected = system.actorSelection('/system/guardian-actor-01');
+    selected[0].tell('ping').subscribe();
+    selected[1].tell('ping ping').subscribe();
+    scheduler.flush();
+});
