106eff71ce18e91f0c501e2fe7c5f51f83c6d081
Shane Osbourne <shane.osbourne8@gmail.com>
2017-07-11 08:14:43 +0100

    assert on methods called

--- test/actor.mailbox.js
+++ test/actor.mailbox.js
@@ -7,9 +7,9 @@ const { SystemActor } = require('../dist/SystemActor');
 const { System } = require('../dist/System');
 const { patterns } = require('../dist');
 
-describe('mailboxes', function() {
+describe.only('mailboxes', function() {
 
-    it.only('works with simple events', function(done) {
+    it('works with simple events', function(done) {
         const system = createSystem();
         const methods = {
             'shane': function(stream) {
@@ -31,14 +31,25 @@ describe('mailboxes', function() {
         const actor = system.actorOf(Child);
         const calls = [];
         actor.ask({type: 'shane', payload: '1'})
-            .subscribe(x => {
-                console.log('1', x);
-            })
+            .subscribe(
+                x => console.log('1 next', x),
+                x => console.log('1 error', x),
+                x => calls.push('1 complete')
+            );
         actor.ask({type: 'shane', payload: '2'})
-            .subscribe(x => {
-                console.log('2', x);
-                done();
-            })
+            .subscribe(
+                x => calls.push('2 next'),
+                x => console.log('2 error', x),
+                x => {
+                    calls.push('2 complete');
+                    assert.deepEqual(calls, [
+                        '1 complete',
+                        '2 next',
+                        '2 complete'
+                    ]);
+                    done();
+                }
+            );
     });
 
     it('an actor can implement switchMap for duplicate messages', function (done) {
